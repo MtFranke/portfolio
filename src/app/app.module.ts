1835d25c21bc6c5d21bd43d58c7e4b5348efd0d4
mateuszfranke <mateuszfranke@icloud.com>
2020-12-06 17:36:48 +0100

    initial components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BiographyComponent } from './biography/biography.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './projects/project/project.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BiographyComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
