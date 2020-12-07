792078638f2c6ee13f2b23dd36f5176bce5eaaef
mateuszfranke <mateuszfranke@icloud.com>
2020-12-07 10:58:59 +0100

    front-end-skills.component finished

--- src/app/front-end-skills/front-end-skills.component.ts
+++ src/app/front-end-skills/front-end-skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SkillModel} from '../model/skill.model';
 
 @Component({
   selector: 'app-front-end-skills',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./front-end-skills.component.scss']
 })
 export class FrontEndSkillsComponent implements OnInit {
+
+  skills: SkillModel[] = [
+    {name: 'React', percentage: 60},
+    {name: 'Javascript', percentage: 80},
+    {name: 'CSS', percentage: 90},
+    {name: 'VUE', percentage: 40},
+    {name: 'Redux', percentage: 80},
+    {name: 'React Native', percentage: 90}
+    ];
 
   constructor() { }
 
