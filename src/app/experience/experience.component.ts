af8f09bdb3e3b2edb6a3a91f30e65ca86edc0287
mateuszfranke <mateuszfranke@icloud.com>
2020-12-08 09:22:36 +0100

    experience component finished

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ExperienceModel} from '../model/experience.model';
 
 @Component({
   selector: 'app-experience',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  experiences: ExperienceModel[] = [
+    {
+      experienceDate: 'Feb 2019 - Current',
+      experienceJobPosition: 'Front-end developer',
+      experienceCompanyLogo: '../../assets/addidas.png',
+      experienceDescription: 'Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.'
+    },
+    {
+      experienceDate: 'Aug 2016 - Feb 2018',
+      experienceJobPosition: 'Full-stack developer',
+      experienceCompanyLogo: '../../assets/hm.png',
+      experienceDescription: 'Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.'
+    },
+    {
+      experienceDate: 'Jun 2015 - Aug 2016',
+      experienceJobPosition: 'Junior front-end developer',
+      experienceCompanyLogo: '../../assets/nivea.png',
+      experienceDescription: 'Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.'
+    }
+
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
